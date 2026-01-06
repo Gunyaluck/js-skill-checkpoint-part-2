478dacf63edf6ca3d553e6e978787ae80976ab32
MOGunyaluck <gunyaluck0619@gmail.com>
2026-01-06 14:18:28 +0700

    refactor: fix question-3

--- question-3.js
+++ question-3.js
@@ -3,19 +3,27 @@
 //รูปแบบ Array ของชื่อที่มีความยาวตัวอักษรมากกว่า 17 ตัวอักษร >> array.map()?ต้องเอาข้อมูลแค่nameออกมาก่อนไหม?, array.filter(>17)
 
 async function getUsers(){
-    const reponse = await fetch("https://jsonplaceholder.typicode.com/users");
-    let data = await reponse.json();
-    return data;
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        let data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        throw error;
+    }
 }
 
-getUsers();
+(async () => {
+    try {
+        const users = await getUsers();// array ที่ดึงมาจาก sever
+        // console.log(users);
+        const nameLetterMoreThan17 = users
+    .map((user) => user.name)
+    .filter((user) => user.length >17);
 
-let users = await getUsers(); // array ที่ดึงมาจาก sever
-// console.log(users);
 
-const nameLetterMoreThan17 = users
-.map((user) => user.name)
-.filter((user) => user.length >17);
-
-
-console.log(nameLetterMoreThan17);
+    console.log(nameLetterMoreThan17);
+    } catch (error) {
+        console.error("เกิดข้อผิดพลาด:", error);
+    }
+})();
